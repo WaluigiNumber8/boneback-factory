f55a3dbf8e5274273b3db5579af15022b3612e0f
Jan Kunetka <>
2021-08-28 20:53:10 +0200

    Asset Type Selection Menu Basics Completed.

--- Assets/Boubak Productions/Scripts/UI/Flexible Grid/FlexibleGridGroup.cs
+++ Assets/Boubak Productions/Scripts/UI/Flexible Grid/FlexibleGridGroup.cs
@@ -5,26 +5,54 @@ using UnityEngine.UI;
 
 public class FlexibleGridGroup : LayoutGroup
 {
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+
+    [SerializeField] FitType fitType;
     [SerializeField] private int rows;
     [SerializeField] private int columns;
     [SerializeField] private Vector2 cellSize;
+    [SerializeField] private Vector2 spacing;
+    public bool fitX;
+    [SerializeField] private bool fitY;
 
     public override void CalculateLayoutInputHorizontal()
     {
         base.CalculateLayoutInputHorizontal();
 
-        float sqrt = Mathf.Sqrt(transform.childCount);
-        rows = Mathf.CeilToInt(sqrt);
-        columns = Mathf.CeilToInt(sqrt);
+        if (fitType == FitType.Uniform || fitType == FitType.Width || fitType == FitType.Height)
+        {
+            fitX = true;
+            fitY = true;
+
+            float sqrt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrt);
+            columns = Mathf.CeilToInt(sqrt);
+        }
+
+        if (fitType == FitType.Width || fitType == FitType.FixedColumns)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        else if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
 
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
 
-        float cellWidth = parentWidth / (float)rows;
-        float cellHeight = parentHeight / (float)columns;
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * 2) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
 
-        cellSize.x = cellWidth;
-        cellSize.y = cellHeight;
+        cellSize.x = (fitX) ? cellWidth : cellSize.x;
+        cellSize.y = (fitY) ? cellHeight : cellSize.y;
 
         int columnCount, rowCount;
         for (int i = 0; i < rectChildren.Count; i++)
@@ -33,8 +61,8 @@ public class FlexibleGridGroup : LayoutGroup
             columnCount = i % columns;
 
             RectTransform child = rectChildren[i];
-            float posX = (cellSize.x * columnCount);
-            float posY = (cellSize.y * rowCount);
+            float posX = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            float posY = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
 
             SetChildAlongAxis(child, 0, posX, cellSize.x);
             SetChildAlongAxis(child, 1, posY, cellSize.y);
