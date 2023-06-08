0c2e41e6543b179dd5a9b1cb1d8b25efc87bf6ce
shyguy255@seznam.cz <>
2023-06-08 18:31:26 +0200

    BBF-149 Optimized SliderWithInput
    * Reworked most of the SliderWithInput class to make more sense and removed useless functionality.

--- Assets/RedRat Productions/Scripts/Runtime/UI/Interactables/Sliders/SliderWithInput.cs
+++ Assets/RedRat Productions/Scripts/Runtime/UI/Interactables/Sliders/SliderWithInput.cs
@@ -13,94 +13,88 @@ namespace RedRats.UI.Sliders
     public class SliderWithInput : MonoBehaviour
     {
         public event Action<float> OnValueChanged;
-        
+
         [SerializeField] private TMP_InputField inputField;
-        
+
         private Slider slider;
-        private bool ignoreValueChange;
-        
+        private bool changingValue;
+
         private void Awake()
         {
             if (inputField.characterValidation != CharacterValidation.Integer &&
                 inputField.characterValidation != CharacterValidation.Decimal &&
                 inputField.characterValidation != CharacterValidation.Digit)
                 throw new InvalidOperationException("Cannot work with non-numeric input fields! Change validation to numbers only.");
-            
+
             slider = GetComponent<Slider>();
         }
 
         private void OnEnable()
         {
-            slider.onValueChanged.AddListener(SetSameValue);
-            inputField.onValueChanged.AddListener(UpdateValue);
-            inputField.onEndEdit.AddListener(FixValue);
+            slider.onValueChanged.AddListener(SetToInputField);
+            inputField.onValueChanged.AddListener(SetToSlider);
         }
 
         private void OnDisable()
         {
-            slider.onValueChanged.RemoveListener(SetSameValue);
-            inputField.onValueChanged.RemoveListener(UpdateValue);
-            inputField.onEndEdit.RemoveListener(FixValue);
+            slider.onValueChanged.RemoveListener(SetToInputField);
+            inputField.onValueChanged.RemoveListener(SetToSlider);
         }
 
         /// <summary>
         /// Sets a new value.
         /// </summary>
         /// <param name="value">The new value.</param>
-        public void ChangeValue(float value)
+        public void SetValue(float value)
         {
-            ignoreValueChange = true;
+            if (changingValue) return;
+
+            changingValue = true;
             slider.value = value;
+            inputField.text = value.ToString();
+            changingValue = false;
+
+            OnValueChanged?.Invoke(value);
         }
 
         /// <summary>
-        /// Updates the value of the Slider and Input Field based on the input value & limits. 
+        /// Set a value into the attached input field only.
         /// </summary>
-        /// <param name="value">The value to enter.</param>
-        private void UpdateValue(string value)
+        /// <param name="value">The value to set.</param>
+        private void SetToInputField(float value)
         {
-            if (EqualsToSpecialSymbol(value)) return;
-            UpdateValue(float.Parse(value));
-        }
-        /// <summary>
-        /// Updates the value of the Slider and Input Field based on the input value & limits. 
-        /// </summary>
-        /// <param name="value">The value to enter.</param>
-        private void UpdateValue(float value)
-        {
-            value = Mathf.Clamp(value, slider.minValue, slider.maxValue);
-            SetSameValue(value);
+            if (changingValue) return;
+
+            changingValue = true;
+            inputField.text = value.ToString();
+            changingValue = false;
+
+            OnValueChanged?.Invoke(value);
         }
 
         /// <summary>
-        /// Sets the value to 0, if it is equal to a special symbol.
+        /// Set a value to the slider only.
         /// </summary>
-        /// <param name="value">The value to check.</param>
-        private void FixValue(string value)
+        /// <param name="stringValue">The value to set.</param>
+        private void SetToSlider(string stringValue)
         {
-            if (!EqualsToSpecialSymbol(value)) return;
-            SetSameValue(0);
+            if (changingValue) return;
+            if (EqualsToSpecialSymbol(stringValue)) return;
+
+            changingValue = true;
+            float value = Mathf.Clamp(float.Parse(stringValue), slider.minValue, slider.maxValue);
+            slider.value = value;
+            inputField.text = value.ToString();
+            changingValue = false;
+
+            OnValueChanged?.Invoke(value);
         }
-        
+
         /// <summary>
         /// Checks if a value is equal to special symbols or not.
         /// </summary>
         /// <param name="value">The value of the Input Field.</param>
         /// <returns>Is true if is equal to any special value.</returns>
         private bool EqualsToSpecialSymbol(string value) => value == "-";
-
-        /// <summary>
-        /// Sets a same value to both the Slider & the Input Field.
-        /// </summary>
-        /// <param name="value">The value to set.</param>
-        private void SetSameValue(float value)
-        {
-            inputField.text = value.ToString();
-            slider.value = value;
-
-            if (ignoreValueChange) { ignoreValueChange = false; return; }
-            
-            OnValueChanged?.Invoke(value);
-        }
     }
 }
