e8da2b379005a0a4f8cbcaa9cfc8e5aafbedcf2c
shyguy255@seznam.cz <>
2023-06-28 14:31:32 +0200

    BBF-67 Added emojis into the new error window
    * Created 4 new emojis of all current interactable objects.
    * The Starting Point and Exit  Gate Error Window now has those emojis present.
    * Default font is now Perftect DOS

--- Assets/Rogium Legend/Scripts/Runtime/Systems/GAS/GASButtonActions.cs
+++ Assets/Rogium Legend/Scripts/Runtime/Systems/GAS/GASButtonActions.cs
@@ -547,7 +547,7 @@ namespace Rogium.Systems.GASExtension
             RoomAsset currentAsset = RoomEditorOverseer.Instance.CurrentAsset;
             if (!currentAsset.ObjectGrid.Contains(AssetDataBuilder.ForObject(InternalLibraryOverseer.GetInstance().GetObjectByID("001"))) || !currentAsset.ObjectGrid.Contains(AssetDataBuilder.ForObject(InternalLibraryOverseer.GetInstance().GetObjectByID("002"))))
             {
-                ModalWindowOverseerMono.GetInstance().OpenWindow(new MessageWindowInfo("Cannot save room until it has at least 1 Exit Gate and 1 Starting Point.", "OK"));
+                ModalWindowOverseerMono.GetInstance().OpenWindow(new MessageWindowInfo("In order to save, there must be at least 1 <style=\"ExitGate\"> Exit Gate</style> and <style=\"StartingPoint\">Starting Point</style> present.", "OK"));
                 return;
             }
             RoomEditorOverseer.Instance.CompleteEditing();
