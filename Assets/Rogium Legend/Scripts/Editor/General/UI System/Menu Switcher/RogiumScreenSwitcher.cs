cafedaaebd8224fa3ce87de1afda34dbdffcddc9
shyguy255@seznam.cz <>
2023-07-02 21:34:26 +0200

    BBF-227 Backgrounds switch with menus
    * When a menu is switched,, the background changes appropriately.
    * Moved UI objects from a struct to the main class.

--- Assets/Rogium Legend/Scripts/Editor/General/UI System/Menu Switcher/RogiumScreenSwitcher.cs
+++ Assets/Rogium Legend/Scripts/Editor/General/UI System/Menu Switcher/RogiumScreenSwitcher.cs
@@ -19,37 +19,67 @@ namespace Rogium.Editor.UI
 
         [Title("Main Menus")] 
         [OnInspectorGUI] private void S1() => GUILayout.Space(0);
-        [ResponsiveButtonGroup("Main"), Button, GUIColor(1f, 0.5f, 0.5f)] public void MainMenu() => Select(editorObjects.mainMenuUI);
-        [ResponsiveButtonGroup("Main"), Button, GUIColor(1f, 0.5f, 0.5f)] public void OptionsMenu() => Select(editorObjects.optionsMenuUI);
-        [ResponsiveButtonGroup("Main"), Button, GUIColor(1f, 0.5f, 0.5f)] public void Changelog() => Select(editorObjects.changelogUI);
+        [ResponsiveButtonGroup("Main"), Button(ButtonSizes.Large), GUIColor(1f, 0.5f, 0.5f)] public void MainMenu() => Select(mainMenuUI, menuBackground);
+        [ResponsiveButtonGroup("Main"), Button(ButtonSizes.Large), GUIColor(1f, 0.5f, 0.5f)] public void OptionsMenu() => Select(optionsMenuUI, menuBackground);
+        [ResponsiveButtonGroup("Main"), Button(ButtonSizes.Large), GUIColor(1f, 0.5f, 0.5f)] public void Changelog() => Select(changelogUI, menuBackground);
         [OnInspectorGUI] private void S2() => GUILayout.Space(8);
         
         [Title("Game Menus")]
         [OnInspectorGUI] private void S3() => GUILayout.Space(0);
-        [ResponsiveButtonGroup("Game"), Button, GUIColor(1f, 0.45f, 0.15f)] public void CampaignSelection() => Select(editorObjects.campaignSelectionMenuUI);
-        [ResponsiveButtonGroup("Game"), Button, GUIColor(1f, 0.45f, 0.15f)] public void CampaignEditor() => Select(editorObjects.campaignEditorUI);
+        [ResponsiveButtonGroup("Game"), Button(ButtonSizes.Large), GUIColor(1f, 0.45f, 0.15f)] public void CampaignSelection() => Select(campaignSelectionMenuUI, gameBackground);
+        [ResponsiveButtonGroup("Game"), Button(ButtonSizes.Large), GUIColor(1f, 0.45f, 0.15f)] public void CampaignEditor() => Select(campaignEditorUI, gameBackground);
         [OnInspectorGUI] private void S4() => GUILayout.Space(8);
         
         [Title("Editor Menus")]
         [OnInspectorGUI] private void S5() => GUILayout.Space(0);
-        [ResponsiveButtonGroup("Editor", UniformLayout = true), Button, GUIColor(0.4f, 0.4f, 1f)] public void SelectionMenu() => Select(editorObjects.selectionMenuUI);
-        [ResponsiveButtonGroup("Editor"), Button, GUIColor(0.4f, 0.4f, 1f)] public void PaletteEditor() => Select(editorObjects.paletteEditorUI);
-        [ResponsiveButtonGroup("Editor"), Button, GUIColor(0.4f, 0.4f, 1f)] public void SpriteEditor() => Select(editorObjects.spriteEditorUI);
-        [ResponsiveButtonGroup("Editor"), Button, GUIColor(0.4f, 0.4f, 1f)] public void PropertyEditor() => Select(editorObjects.propertyEditorUI);
-        [ResponsiveButtonGroup("Editor"), Button, GUIColor(0.4f, 0.4f, 1f)] public void RoomEditor() => Select(editorObjects.roomEditorUI);
+        [ResponsiveButtonGroup("Editor"), Button(ButtonSizes.Large), GUIColor(0.4f, 0.4f, 1f)] public void SelectionMenu() => Select(selectionMenuUI, editorBackground);
+        [ResponsiveButtonGroup("Editor"), Button(ButtonSizes.Large), GUIColor(0.4f, 0.4f, 1f)] public void PaletteEditor() => Select(paletteEditorUI, editorBackground);
+        [ResponsiveButtonGroup("Editor"), Button(ButtonSizes.Large), GUIColor(0.4f, 0.4f, 1f)] public void SpriteEditor() => Select(spriteEditorUI, editorBackground);
+        [ResponsiveButtonGroup("Editor"), Button(ButtonSizes.Large), GUIColor(0.4f, 0.4f, 1f)] public void PropertyEditor() => Select(propertyEditorUI, editorBackground);
+        [ResponsiveButtonGroup("Editor"), Button(ButtonSizes.Large), GUIColor(0.4f, 0.4f, 1f)] public void RoomEditor() => Select(roomEditorUI, editorBackground);
         
-        public MenuInfo editorObjects;
+        [FoldoutGroup("EditorObjects")]public GameObject mainMenuUI;
+        [FoldoutGroup("EditorObjects")]public GameObject optionsMenuUI;
+        [FoldoutGroup("EditorObjects")]public GameObject changelogUI;
+        [FoldoutGroup("EditorObjects")]public GameObject menuBackground;
+        [Space]
+        [FoldoutGroup("EditorObjects")]public GameObject campaignSelectionMenuUI;
+        [FoldoutGroup("EditorObjects")]public GameObject campaignEditorUI;
+        [FoldoutGroup("EditorObjects")]public GameObject gameBackground;
+        [Space]
+        [FoldoutGroup("EditorObjects")]public GameObject selectionMenuUI;
+        [FoldoutGroup("EditorObjects")]public GameObject paletteEditorUI;
+        [FoldoutGroup("EditorObjects")]public GameObject spriteEditorUI;
+        [FoldoutGroup("EditorObjects")]public GameObject propertyEditorUI;
+        [FoldoutGroup("EditorObjects")]public GameObject roomEditorUI;
+        [FoldoutGroup("EditorObjects")]public GameObject editorBackground;
 
         private GameObject lastObject;
-        
+
+        protected override void OnEnable()
+        {
+            string data = EditorPrefs.GetString("RogiumMenuSwitcher", JsonUtility.ToJson(this));
+            JsonUtility.FromJsonOverwrite(data, this);
+            base.OnEnable();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            string data = JsonUtility.ToJson(this, false);
+            EditorPrefs.SetString("RogiumMenuSwitcher", data);
+        }
+
         /// <summary>
         /// Activates a specific menu.
         /// </summary>
         /// <param name="menu">The menu object to activate.</param>
-        private void Select(GameObject menu)
+        /// <param name="background">The background the editor uses.</param>
+        private void Select(GameObject menu, GameObject background)
         {
             DeselectAll();
             menu.SetActive(true);
+            background.SetActive(true);
             SceneHierarchyController.Select((menu.transform.childCount <= 0) ? menu : menu.transform.GetChild(0).gameObject);
             lastObject = menu;
         }
@@ -61,37 +91,21 @@ namespace Rogium.Editor.UI
         {
             if (lastObject != null) SceneHierarchyController.SetExpanded(lastObject.transform.parent.gameObject, false);
             
-            editorObjects.mainMenuUI.SetActive(false);
-            editorObjects.optionsMenuUI.SetActive(false);
-            editorObjects.changelogUI.SetActive(false);
+            mainMenuUI.SetActive(false);
+            optionsMenuUI.SetActive(false);
+            changelogUI.SetActive(false);
+            menuBackground.SetActive(false);
             
-            editorObjects.campaignSelectionMenuUI.SetActive(false);
-            editorObjects.campaignEditorUI.SetActive(false);
+            campaignSelectionMenuUI.SetActive(false);
+            campaignEditorUI.SetActive(false);
+            gameBackground.SetActive(false);
             
-            editorObjects.selectionMenuUI.SetActive(false);
-            editorObjects.paletteEditorUI.SetActive(false);
-            editorObjects.spriteEditorUI.SetActive(false);
-            editorObjects.propertyEditorUI.SetActive(false);
-            editorObjects.roomEditorUI.SetActive(false);
+            selectionMenuUI.SetActive(false);
+            paletteEditorUI.SetActive(false);
+            spriteEditorUI.SetActive(false);
+            propertyEditorUI.SetActive(false);
+            roomEditorUI.SetActive(false);
+            editorBackground.SetActive(false);
         }
-        
-        [Serializable]
-        public struct MenuInfo
-        {
-            public GameObject mainMenuUI;
-            public GameObject optionsMenuUI;
-            public GameObject changelogUI;
-            [Space]
-            public GameObject campaignSelectionMenuUI;
-            public GameObject campaignEditorUI;
-            [Space]
-            
-            public GameObject selectionMenuUI;
-            public GameObject paletteEditorUI;
-            public GameObject spriteEditorUI;
-            public GameObject propertyEditorUI;
-            public GameObject roomEditorUI;
-        }
-        
     }
 }
