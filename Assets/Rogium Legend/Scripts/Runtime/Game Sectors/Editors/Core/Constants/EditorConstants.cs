c21815648754be94e157ba9be560d196fcebc6ae
shyguy255@seznam.cz <>
2023-07-31 19:47:47 +0200

    BBF-160 Added sprite for enemy
    * Added a default sprite for new enemies.

--- Assets/Rogium Legend/Scripts/Runtime/Game Sectors/Editors/Core/Constants/EditorConstants.cs
+++ Assets/Rogium Legend/Scripts/Runtime/Game Sectors/Editors/Core/Constants/EditorConstants.cs
@@ -96,7 +96,7 @@ namespace Rogium.Editors.Core.Defaults
         
         //Enemy
         public const string EnemyTitle = "New Enemy";
-        public static readonly Sprite EnemyIcon = Resources.Load<Sprite>("Defaults/spr_TestDummy");
+        public static readonly Sprite EnemyIcon = Resources.Load<Sprite>("Defaults/spr_Default_Enemy");
 
         public const AnimationType EnemyAnimationType = AnimationType.HorizontalFlip;
         public const int EnemyFrameDuration = 50;
