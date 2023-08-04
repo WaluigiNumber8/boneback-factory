40c1cad9e0ec0c5b26717b2a52227ff2f7be8ac7
shyguy255@seznam.cz <>
2023-08-04 12:06:03 +0200

    BBF-229 Adjusted Main Menu & Campaign Selection for QHD resolution
    * The elements in the main menu & campaign selection menu are adjusted for QHD
    * Adjusted Campaign Selection Button layout.

--- Assets/Rogium Legend/Scripts/Runtime/Systems/Grid System/Interactable Grid/Grid Previewer/GridVisualPreviewer.cs
+++ Assets/Rogium Legend/Scripts/Runtime/Systems/Grid System/Interactable Grid/Grid Previewer/GridVisualPreviewer.cs
@@ -99,7 +99,6 @@ namespace Rogium.Systems.GridSystem
         private void UpdatePositionOnGrid()
         {
             if (!gridPreviewer.gameObject.activeSelf) return;
-            
             float x = gridTransform.position.x + grid.SelectedPosition.x * grid.CellSize.x + grid.CellSize.x * 0.5f;
             float y = gridTransform.position.y + grid.SelectedPosition.y * grid.CellSize.y + grid.CellSize.y * 0.5f;
             gridPreviewer.transform.position = new Vector3(x, y);
