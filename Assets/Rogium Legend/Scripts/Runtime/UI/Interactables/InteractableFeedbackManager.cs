e0eabff45cac55841f6d08e254645857cad00979
shyguy255@seznam.cz <>
2023-09-20 10:28:38 +0200

    BBF-235 Added Anim+SFX to basic buttons
    * Buttons of type Menu now have animations and sfx when interacted with.
    * Reorginized the audio folder.

---
+++ Assets/Rogium Legend/Scripts/Runtime/UI/Interactables/InteractableFeedbackManager.cs
@@ -0,0 +1,32 @@
+﻿using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace Rogium.UserInterface.Interactables
+{
+    /// <summary>
+    /// Associates <see cref="MMF_Player"/>s with a UI component.
+    /// </summary>
+    public class InteractableFeedbackManager : MonoBehaviour
+    {
+        [SerializeField] private InteractableEventCaller interactable;
+        [Space]
+        [SerializeField] private MMF_Player onClickPlayer;
+        [SerializeField] private MMF_Player onSelectPlayer;
+        [SerializeField] private MMF_Player onDeselectPlayer;
+
+        private void OnEnable()
+        {
+            interactable.OnClick += onClickPlayer.PlayFeedbacks;
+            interactable.OnSelect += onSelectPlayer.PlayFeedbacks;
+            interactable.OnDeselect += onDeselectPlayer.PlayFeedbacks;
+        }
+
+        private void OnDisable()
+        {
+            interactable.OnClick -= onClickPlayer.PlayFeedbacks;
+            interactable.OnSelect -= onSelectPlayer.PlayFeedbacks;
+            interactable.OnDeselect -= onDeselectPlayer.PlayFeedbacks;
+        }
+
+    }
+}
