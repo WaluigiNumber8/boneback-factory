6c28adf5ba630c73271890dbb7c289f2494737fc
shyguy255@seznam.cz <>
2023-09-20 09:32:29 +0200

    BBF-235 Imported FEEL package

---
+++ Assets/Rogium Legend/Scripts/Runtime/UI/Interactables/InteractableEventCaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Rogium.UserInterface.Interactables
+{
+    /// <summary>
+    /// Calls out different events for a <see cref="Selectable"/>.
+    /// </summary>
+    [RequireComponent(typeof(Selectable))]
+    public class InteractableEventCaller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, ISelectHandler, IDeselectHandler, ISubmitHandler
+    {
+        public event Action OnClick;
+        public event Action OnSelect;
+        public event Action OnDeselect;
+
+        public void OnPointerEnter(PointerEventData eventData) => OnSelect?.Invoke();
+        public void OnPointerExit(PointerEventData eventData) => OnDeselect?.Invoke();
+        public void OnPointerDown(PointerEventData eventData) => OnClick?.Invoke();
+        void ISelectHandler.OnSelect(BaseEventData eventData) => OnSelect?.Invoke();
+        void IDeselectHandler.OnDeselect(BaseEventData eventData) => OnDeselect?.Invoke();
+        public void OnSubmit(BaseEventData eventData) => OnClick?.Invoke();
+    }
+}
