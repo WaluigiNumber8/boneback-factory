cb79348da4c6d361d083436de84835cf5468e748
shyguy255@seznam.cz <>
2023-10-09 20:35:57 +0200

    BBF-239 Added sfx+anims to sliders
    * Sliders now have proper animations and sound effects
    * A sound plays when the slider is dragged. The pitch of the feedbacks sound can change based on the slider's value.
    * Moved InteractableEventCaller and InteractableFeedbackManager to FEEL Extensions where they make more sense.

--- Assets/Feel Extensions/Interactable Feedbacks/InteractableEventCaller.cs
+++ Assets/Feel Extensions/Interactable Feedbacks/InteractableEventCaller.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace Rogium.UserInterface.Interactables
+namespace Rogium.UserInterface.Interactables.Feedbacks
 {
     /// <summary>
     /// Calls out different events for a <see cref="Selectable"/>.
     /// </summary>
     [RequireComponent(typeof(Selectable))]
-    public class InteractableEventCaller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, ISelectHandler, IDeselectHandler, ISubmitHandler
+    public class InteractableEventCaller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, ISelectHandler, IDeselectHandler, ISubmitHandler
     {
         public event Action OnClick;
         public event Action OnSelect;
         public event Action OnDeselect;
+        public event Action OnClickDown;
+        public event Action OnClickUp;
 
         public void OnPointerEnter(PointerEventData eventData) => WhenSelect();
         public void OnPointerExit(PointerEventData eventData) => WhenDeselect();
         public void OnPointerDown(PointerEventData eventData) => WhenClick();
+        public void OnPointerUp(PointerEventData eventData) => OnClickUp?.Invoke();
         void ISelectHandler.OnSelect(BaseEventData eventData) => WhenSelect();
         void IDeselectHandler.OnDeselect(BaseEventData eventData) => WhenDeselect();
         public void OnSubmit(BaseEventData eventData) => WhenClick();
@@ -33,7 +36,11 @@ namespace Rogium.UserInterface.Interactables
         /// <summary>
         /// Is called when the interactable is clicked.
         /// </summary>
-        protected virtual void WhenClick() => OnClick?.Invoke();
-        
+        protected virtual void WhenClick()
+        {
+            OnClick?.Invoke();
+            OnClickDown?.Invoke();
+        }
+
     }
 }
