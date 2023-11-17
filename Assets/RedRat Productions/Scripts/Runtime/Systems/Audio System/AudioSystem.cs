3fe50e1eedc6a79807dcff05ad58e4cfa63facfe
shyguy255@seznam.cz <>
2023-11-17 10:48:55 +0100

    BBF-274 Added a volume slider to AudioEffect to control volume.

--- Assets/RedRat Productions/Scripts/Runtime/Systems/Audio System/AudioSystem.cs
+++ Assets/RedRat Productions/Scripts/Runtime/Systems/Audio System/AudioSystem.cs
@@ -37,11 +37,11 @@ namespace RedRats.Systems.Audio
         /// <param name="mixerGroup">Which group the sound played</param>
         /// <param name="id">The id of the source to use. If 0 is used, a random <see cref="AudioSource"/> will be picked instead.</param>
         /// <param name="playOnlyWhenNotPlaying">Only play the given clip if it's not being played already.</param>
+        /// <param name="volume">How loud the sound is.</param>
         /// <param name="pitchMin">Minimum allowed pitch.</param>
         /// <param name="pitchMax">Maximum allowed pitch.</param>
-        public void PlaySound(AudioClip[] clips, AudioMixerGroup mixerGroup, int id,
-            bool playOnlyWhenNotPlaying = false,
-            float pitchMin = 1, float pitchMax = 1)
+        public void PlaySound(AudioClip[] clips, AudioMixerGroup mixerGroup, int id, bool playOnlyWhenNotPlaying = false,
+            float volume = 1, float pitchMin = 1, float pitchMax = 1)
         {
             AudioClip clip = clips[Random.Range(0, clips.Length)];
 
@@ -51,6 +51,7 @@ namespace RedRats.Systems.Audio
             AudioSource source = (id == 0) ? sourcePool.Get() : sourcePool.Get(id);
             source.clip = clip;
             source.outputAudioMixerGroup = mixerGroup;
+            source.volume = volume;
             source.pitch = Random.Range(pitchMin, pitchMax);
             source.Play();
 
--- Assets/RedRat Productions/Scripts/Runtime/Systems/Effector System/Effects/AudioEffect.cs
+++ Assets/RedRat Productions/Scripts/Runtime/Systems/Effector System/Effects/AudioEffect.cs
@@ -9,21 +9,28 @@ namespace RedRats.Systems.Effectors.Effects
     /// </summary>
     public class AudioEffect : EffectBase
     {
+        [Header("Clips")]
         [SerializeField] private AudioClip[] clips;
+        
+        [Header("Mixing")]
         [SerializeField] private AudioMixerGroup mixerGroup;
         [SerializeField] private int id;
         [SerializeField] private bool playOnlyWhenNotPlaying;
         
-        [SerializeField, Range(0f, 2f)] private float pitchMin = 1;
-        [SerializeField, Range(0f, 2f)] private float pitchMax = 1;
+        [Header("Settings")]
+        [SerializeField, Range(0f, 1f)] private float volume = 1f;
+        [SerializeField, Range(0f, 2f)] private float pitchMin = 1f;
+        [SerializeField, Range(0f, 2f)] private float pitchMax = 1f;
+        private AudioSystem audioSystem;
 
+        private void Awake() => audioSystem = AudioSystem.GetInstance();
 
         /// <summary>
         /// Plays the audio.
         /// </summary>
         protected override void PlayEffects()
         {
-            AudioSystem.GetInstance().PlaySound(clips, mixerGroup, id, playOnlyWhenNotPlaying, pitchMin, pitchMax);
+            audioSystem.PlaySound(clips, mixerGroup, id, playOnlyWhenNotPlaying, volume, pitchMin, pitchMax);
         }
     }
 }
