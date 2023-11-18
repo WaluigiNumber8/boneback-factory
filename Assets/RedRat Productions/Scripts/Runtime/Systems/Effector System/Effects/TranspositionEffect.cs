3e50649db5030cc23fb6c02a3603dd74085454d3
shyguy255@seznam.cz <>
2023-11-18 13:13:19 +0100

    BBF-275 Removed Tooltips+variable comments.

--- Assets/RedRat Productions/Scripts/Runtime/Systems/Effector System/Effects/TranspositionEffect.cs
+++ Assets/RedRat Productions/Scripts/Runtime/Systems/Effector System/Effects/TranspositionEffect.cs
@@ -10,67 +10,22 @@ namespace RedRats.Systems.Effectors.Effects
     /// </summary>
     public class TranspositionEffect : EffectBase
     {
-        /// <summary>
-        /// The target to move.
-        /// </summary>
-        [Tooltip("The target to move.")] 
         [Header("Target")]
         [SerializeField] private Transform target;
-        /// <summary>
-        /// How long will the movement last.
-        /// </summary>
-        [Tooltip("How long will the movement last.")] 
         [SerializeField] private float duration = 0.2f;
-        /// <summary>
-        /// Restart the movement from beginning when replaying.
-        /// </summary>
-        [Tooltip("Restart the movement from beginning when replaying.")] 
         [SerializeField] private bool restartOnReplay = true;
-        /// <summary>
-        /// Will the target move back to it's original position smoothly when stopped?
-        /// </summary>
-        [Tooltip("Will the target move back to it's original position smoothly when stopped?")] 
         [SerializeField] private bool smoothReset;
-
-        /// <summary>
-        /// The position the target has to reach.
-        /// </summary>
-        [Tooltip("The position the target has to reach.")] 
+        
         [Header("Transposition")]
         [SerializeField] private Vector3 endPosition;
-        /// <summary>
-        /// The easing to use for the movement.
-        /// </summary>
-        [Tooltip("The easing to use for the movement.")] 
         [SerializeField] private Ease easing = Ease.InOutSine;
-        /// <summary>
-        /// Is the movement going to happen in absolute or relative coordinates?
-        /// </summary>
-        [Tooltip("Is the movement going to happen in absolute or relative coordinates?")] 
         [SerializeField] private MovementType movement = MovementType.Relative;
-        /// <summary>
-        /// is the movement happening in world space or space relative to parent?
-        /// </summary>
-        [Tooltip("is the movement happening in world space or space relative to parent?")] 
         [SerializeField] private WorldType worldType = WorldType.World;
-
-        /// <summary>
-        /// Loop the movement infinitely.
-        /// </summary>
-        [Tooltip("Loop the movement infinitely.")] 
+        
         [Header("Looping")] 
         [SerializeField] private bool infiniteLoop;
-        /// <summary>
-        /// How many times to loop the movement.
-        /// </summary>
-        [Tooltip("How many times to loop the movement.")] [SerializeField, HideIf("infiniteLoop")]
-        private int loops;
-        /// <summary>
-        /// The type of looping to use. RESTART moves target back after each sequence. YOYO moves target back and forth. INCREMENTAL starts the next sequence where the previous one ended.
-        /// </summary>
-        [Tooltip("The type of looping to use. RESTART moves target back after each sequence. YOYO moves target back and forth. INCREMENTAL starts the next sequence where the previous one ended.")]
-        [SerializeField]
-        private LoopType loopType = LoopType.Restart;
+        [SerializeField] private int loops;
+        [SerializeField] private LoopType loopType = LoopType.Restart;
 
         private Vector3 startPosition;
 
