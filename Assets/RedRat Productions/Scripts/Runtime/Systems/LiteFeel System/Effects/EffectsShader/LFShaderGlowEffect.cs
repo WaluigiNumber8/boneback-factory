cc03a8918adcbf36228921fa623c8bd95c5804ab
shyguy255@seznam.cz <>
2024-02-14 12:26:29 +0100

    BBF-283 Simplify Glow shader to instensity and saturation sliders

--- Assets/RedRat Productions/Scripts/Runtime/Systems/LiteFeel System/Effects/EffectsShader/LFShaderGlowEffect.cs
+++ Assets/RedRat Productions/Scripts/Runtime/Systems/LiteFeel System/Effects/EffectsShader/LFShaderGlowEffect.cs
@@ -6,34 +6,48 @@ namespace RedRats.Systems.LiteFeel.Effects
 {
     public class LFShaderGlowEffect : LFShaderBase
     {
-        [Header("Glow")]
-        [SerializeField, ColorUsage(false, true)] private Color beginGlow;
-        [SerializeField, ColorUsage(false, true)] private Color targetGlow;
-        [SerializeField] protected SmoothingType smoothing = SmoothingType.AnimationCurve;
-        [SerializeField, HideIf("smoothing", SmoothingType.AnimationCurve)] protected Ease easing = Ease.InOutSine;
-        [SerializeField, HideIf("smoothing", SmoothingType.Tween)] protected AnimationCurve blendCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
+        [Header("Glow Intensity")]
+        [SerializeField, Range(0f, 12f)] private float beginIntensity = 0;
+        [SerializeField, Range(0f, 12f)] private float targetIntensity = 1;
+        [SerializeField] protected SmoothingType intensitySmoothing = SmoothingType.AnimationCurve;
+        [SerializeField, HideIf("intensitySmoothing", SmoothingType.AnimationCurve)] protected Ease intensityEasing = Ease.InOutSine;
+        [SerializeField, HideIf("intensitySmoothing", SmoothingType.Tween)] protected AnimationCurve intensityCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
         
-        private static readonly int GlowProperty = Shader.PropertyToID("_GlowColor");
-        private Color startGlow;
+        [Header("Glow Saturation")]
+        [SerializeField, Range(0f, 8f)] private float beginSaturation = 1;
+        [SerializeField, Range(0f, 8f)] private float targetSaturation = 2;
+        [SerializeField] protected SmoothingType saturationSmoothing = SmoothingType.AnimationCurve;
+        [SerializeField, HideIf("saturationSmoothing", SmoothingType.AnimationCurve)] protected Ease saturationEasing = Ease.InOutSine;
+        [SerializeField, HideIf("saturationSmoothing", SmoothingType.Tween)] protected AnimationCurve saturationCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
+        
+        private static readonly int IntensityProperty = Shader.PropertyToID("_GlowIntensity");
+        private static readonly int SaturationProperty = Shader.PropertyToID("_GlowSaturation");
+        
+        private float startIntensity;
+        private float startSaturation;
         
         protected override void ResetTargetState()
         {
-            material.SetColor(GlowProperty, startGlow);
+            material.SetFloat(IntensityProperty, startIntensity);
+            material.SetFloat(SaturationProperty, startSaturation);
         }
 
         protected override void UpdateStartingValues()
         {
-            startGlow = material.GetColor(GlowProperty);
+            startIntensity = material.GetFloat(IntensityProperty);
+            startSaturation = material.GetFloat(SaturationProperty);
         }
 
         protected override void SetBeginState()
         {
-            material.SetColor(GlowProperty, beginGlow);
+            material.SetFloat(IntensityProperty, beginIntensity);
+            material.SetFloat(SaturationProperty, beginSaturation);
         }
 
         protected override void SetupTweens()
         {
-            AddColorTween(GlowProperty, targetGlow, smoothing, easing, blendCurve);
+            AddFloatTween(IntensityProperty, targetIntensity, intensitySmoothing, intensityEasing, intensityCurve);
+            AddFloatTween(SaturationProperty, targetSaturation, saturationSmoothing, saturationEasing, saturationCurve);
         }
     }
 }
