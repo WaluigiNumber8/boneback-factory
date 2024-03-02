e697d24546dde6039ae476c36682887cfa5dff0c
shyguy255@seznam.cz <>
2024-03-02 17:45:46 +0100

    BBF-258 Add SoundPicker property class

---
+++ Assets/Rogium Legend/Scripts/Runtime/UI/Interactables/Properties/Types/InteractablePropertySoundPicker.cs
@@ -0,0 +1,14 @@
+namespace Rogium.UserInterface.Interactables.Properties
+{
+    /// <summary>
+    /// Represents a sound picker property.
+    /// </summary>
+    public class InteractablePropertySoundPicker : InteractablePropertyBase
+    {
+        
+        public override void SetDisabled(bool isDisabled)
+        {
+            
+        }
+    }
+}
