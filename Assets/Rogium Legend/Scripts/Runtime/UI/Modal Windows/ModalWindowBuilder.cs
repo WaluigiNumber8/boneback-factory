4e93c80aacc45c258f08963d7718c3b3de353386
shyguy255@seznam.cz <>
2024-03-09 14:12:08 +0100

    BBF-319 Add comments to window opening methods in ModalWindowBuilder

--- Assets/Rogium Legend/Scripts/Runtime/UI/Modal Windows/ModalWindowBuilder.cs
+++ Assets/Rogium Legend/Scripts/Runtime/UI/Modal Windows/ModalWindowBuilder.cs
@@ -35,17 +35,34 @@ namespace Rogium.UserInterface.ModalWindows
             cachedSoundPickerWindow.Close();
         }
 
+        /// <summary>
+        /// Opens a new message window.
+        /// </summary>
+        /// <param name="data">Data to load the window with.</param>
         public void OpenMessageWindow(MessageWindowInfo data)
         {
             windowGenerator.Open(data);
         }
         
+        /// <summary>
+        /// Opens a property window with a single column.
+        /// </summary>
+        /// <param name="data">The data to load the window with.</param>
+        /// <param name="key">Unique key that will identify the window.</param>
+        /// <param name="column1">Transform of the property column.</param>
         public void OpenPropertyWindowColumns1(PropertyWindowInfo data, string key, out Transform column1)
         {
             windowGenerator.Open(data, key);
             column1 = windowGenerator.GetColumn1(key);
         }
         
+        /// <summary>
+        /// Opens a property window with two columns.
+        /// </summary>
+        /// <param name="data">The data to load the window with.</param>
+        /// <param name="key">Unique key that will identify the window.</param>
+        /// <param name="column1">Transform of the 1st column.</param>
+        /// <param name="column2">Transform of the 2nd column.</param>
         public void OpenPropertyWindowColumns2(PropertyWindowInfo data, string key, out Transform column1, out Transform column2)
         {
             windowGenerator.Open(data, key);
