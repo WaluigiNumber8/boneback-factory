498f3ebcefc56aaaa6d00c1c4452c15a4cbbabe8
shyguy255@seznam.cz <>
2024-04-01 20:24:47 +0200

    BBF-331 Add OnEnable animation to basic Modal Window

--- Assets/RedRat Productions/Scripts/Runtime/Systems/LiteFeel System/Effects/Effects UI/LFUICanvasGroupEffect.cs
+++ Assets/RedRat Productions/Scripts/Runtime/Systems/LiteFeel System/Effects/Effects UI/LFUICanvasGroupEffect.cs
@@ -8,23 +8,25 @@ public class LFUICanvasGroupEffect : LFEffectTweenBase
     [SerializeField, InfoBox("Missing target", InfoMessageType.Error, "@target == null")] private CanvasGroup target;
     [SerializeField] private ImageFadeType fade = ImageFadeType.FadeIn;
     [SerializeField] private AnimationCurve fadeCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
+    [SerializeField] private bool blockRaycastsDuringPlay;
     
     private float startAlpha;
     
     protected override void SetBeginState()
     {
-        target.blocksRaycasts = true;
         target.alpha = (fade == ImageFadeType.FadeIn) ? 0f : 1f;
+        if (blockRaycastsDuringPlay) target.blocksRaycasts = true;
     }
 
     protected override void SetupTweens()
     {
         Tween tween = DOTween.To(() => target.alpha, x => target.alpha = x, GetTargetValue(), Duration);
-        tween.OnComplete(() => target.blocksRaycasts = false);
+        if (blockRaycastsDuringPlay) tween.OnComplete(() => target.blocksRaycasts = false);
         AddTweenToSequence(tween, fadeCurve);
     }
 
     protected override void ResetTargetState() => target.alpha = startAlpha;
+
     protected override void UpdateStartingValues() => startAlpha = target.alpha;
 
     private float GetTargetValue() => (fade == ImageFadeType.FadeIn) ? 1f : 0f;
