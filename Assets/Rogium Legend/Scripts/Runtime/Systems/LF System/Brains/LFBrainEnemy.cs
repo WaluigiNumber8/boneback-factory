cc83bec29daa7676b811d38053d0fdf26a8669e7
shyguy255@seznam.cz <>
2024-04-21 11:10:05 +0200

    BBF-270 Refactor  LFEnemyBrain by generalizing  hit particle settings

--- Assets/Rogium Legend/Scripts/Runtime/Systems/LF System/Brains/LFBrainEnemy.cs
+++ Assets/Rogium Legend/Scripts/Runtime/Systems/LF System/Brains/LFBrainEnemy.cs
@@ -1,10 +1,9 @@
-using System;
 using RedRats.Core;
 using RedRats.Systems.LiteFeel.Core;
-using RedRats.Systems.LiteFeel.Effects;
 using Rogium.Gameplay.Entities.Enemy;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Rogium.Systems.LiteFeel.Brains
 {
@@ -18,7 +17,8 @@ namespace Rogium.Systems.LiteFeel.Brains
         [SerializeField, GUIColor(1f, 0.25f, 0f)] private LFEffector onHitEffector;
         [SerializeField, GUIColor(1f, 0.25f, 0f)] private LFEffector onDeathEffector;
         [Space] 
-        [SerializeField] private HitEffectSettingsInfo hitSettings;
+        [SerializeField, FormerlySerializedAs("damageSettings")] private DamageParticleSettingsInfo hitSettings;
+        
         
         private void OnEnable()
         {
@@ -34,30 +34,11 @@ namespace Rogium.Systems.LiteFeel.Brains
         
         private void WhenHit(int damage, Vector3 hitDirection)
         {
-            int amount = GetAmountOfParticles(damage);
+            int amount = RedRatUtils.RemapAndEvaluate(damage, hitSettings.amountCurve, hitSettings.minDamage, hitSettings.maxDamage, hitSettings.minAmount, hitSettings.maxAmount);
             hitSettings.particleEffect.UpdateBurstAmount(0, amount - 1, amount + 1);
             hitSettings.particleEffect.UpdateColor(enemy.RepresentativeColor);
             hitSettings.particleEffect.UpdateRotationOffset(hitDirection);
             onHitEffector.Play();
-        }
-        
-        private int GetAmountOfParticles(int damage)
-        {
-            float dmg = ((float)damage).Remap(hitSettings.minDamage , hitSettings.maxDamage, 0f, 1f);
-            float curveValue = hitSettings.amountCurve.Evaluate(dmg);
-            int amount = (int)curveValue.Remap(0f, 1f, hitSettings.minAmount, hitSettings.maxAmount);
-            return amount;
-        }
-
-        [Serializable]
-        public struct HitEffectSettingsInfo
-        {
-            [Required] public LFParticleEffect particleEffect;
-            public AnimationCurve amountCurve;
-            [MinValue(0)] public int minDamage;
-            [MinValue(0)] public int maxDamage;
-            [MinValue(0)] public int minAmount;
-            [MinValue(0)] public int maxAmount;
         }
     }
 }
---
+++ Assets/Rogium Legend/Scripts/Runtime/Systems/LF System/DamageParticleSettingsInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using RedRats.Systems.LiteFeel.Effects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Rogium.Systems.LiteFeel
+{
+    /// <summary>
+    /// Settings for a damage particle effect, that scales with damage.
+    /// </summary>
+    [Serializable]
+    public struct DamageParticleSettingsInfo
+    {
+        [Required] public LFParticleEffect particleEffect;
+        public AnimationCurve amountCurve;
+        [MinValue(0)] public int minDamage;
+        [MinValue(0)] public int maxDamage;
+        [MinValue(0)] public int minAmount;
+        [MinValue(0)] public int maxAmount;
+    }
+}
