8e7159c4d1115153440d7f8545bb60f37eb51bea
shyguy255@seznam.cz <>
2024-05-06 16:59:13 +0200

    Fix Weapon select menu background & player rendering behind ceiling

---
+++ Assets/RedRat Productions/Scripts/Runtime/Core/Attributes/SortingLayerAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RedRats.Core
+{
+    /// <summary>
+    /// Turns an int into a sorting layer field.
+    /// </summary>
+    public class SortingLayerAttribute : PropertyAttribute { }
+}
