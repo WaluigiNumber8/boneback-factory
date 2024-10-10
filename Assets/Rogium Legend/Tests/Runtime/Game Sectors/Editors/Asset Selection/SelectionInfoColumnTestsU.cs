04c208e48685121af71b366d15f6344e345c9157
shyguy255@seznam.cz <>
2024-10-10 13:16:28 +0200

    BBF-349 Drastically shorten Selection Column test run time

--- Assets/Rogium Legend/Tests/Runtime/Game Sectors/Editors/Asset Selection/SelectionInfoColumnTestsU.cs
+++ Assets/Rogium Legend/Tests/Runtime/Game Sectors/Editors/Asset Selection/SelectionInfoColumnTestsU.cs
@@ -10,11 +10,9 @@ namespace Rogium.Tests.Editors.AssetSelection
         public static IEnumerator OpenPackAndSelectRoom(int packIndex = 0, int roomIndex = 0)
         {
             GASButtonActions.OpenSelectionPack();
-            yield return new WaitForSeconds(1);
             ((EditableAssetCardControllerV2)SelectionMenuOverseerMono.GetInstance().CurrentSelector.GetCard(packIndex)).Edit();
-            yield return new WaitForSeconds(1);
             GASButtonActions.OpenSelectionRoom();
-            yield return new WaitForSeconds(1);
+            yield return null;
             SelectionMenuOverseerMono.GetInstance().CurrentSelector.GetCard(roomIndex).SetToggle(true);
             yield return null;
         }
@@ -22,9 +20,8 @@ namespace Rogium.Tests.Editors.AssetSelection
         public static IEnumerator OpenPackAndSelectPalette()
         {
             GASButtonActions.OpenSelectionPack();
-            yield return new WaitForSeconds(1);
             GASButtonActions.OpenEditor(0);
-            yield return new WaitForSeconds(1);
+            yield return null;
             SelectionMenuOverseerMono.GetInstance().CurrentSelector.GetCard(0).SetToggle(true);
             yield return null;
         }
