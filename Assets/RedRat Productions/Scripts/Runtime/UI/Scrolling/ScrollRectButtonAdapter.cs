8809f68f009e4211aa75358d6207066c10cb2638
shyguy255@seznam.cz <>
2024-10-20 13:20:45 +0200

    BBF-371 Can scroll left with button
    also hide buttons when on scroll rect edges

--- Assets/RedRat Productions/Scripts/Runtime/UI/Scrolling/ScrollRectButtonAdapter.cs
+++ Assets/RedRat Productions/Scripts/Runtime/UI/Scrolling/ScrollRectButtonAdapter.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using RedRats.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,35 +14,86 @@ namespace RedRats.UI.Core.Scrolling
     {
         [SerializeField] private float scrollSpeed = 0.01f;
         [SerializeField] private HoldableButton scrollRightButton;
+        [SerializeField] private HoldableButton scrollLeftButton;
         
         private ScrollRect scrollRect;
-        private Action scrollAction;
+        private IDictionary<DirectionType, ScrollInfo> scrollData;
+        private ScrollInfo? currentScroll;
         
-        private void Awake() => scrollRect = GetComponent<ScrollRect>();
+        private void Awake()
+        {
+            scrollRect = GetComponent<ScrollRect>();
+            scrollData = new Dictionary<DirectionType, ScrollInfo>
+            {
+                {DirectionType.Right, new ScrollInfo(scrollRightButton, () => scrollRect.horizontalNormalizedPosition += scrollSpeed, () => scrollRect.normalizedPosition.x >= 1f)},
+                {DirectionType.Left, new ScrollInfo(scrollLeftButton, () => scrollRect.horizontalNormalizedPosition -= scrollSpeed, () => scrollRect.normalizedPosition.x <= 0f)}
+            };
+        }
 
         private void OnEnable()
         {
             scrollRightButton.OnHoldStart += ScrollRight;
-            
             scrollRightButton.OnHoldEnd += StopScrolling;
+            scrollLeftButton.OnHoldStart += ScrollLeft;
+            scrollLeftButton.OnHoldEnd += StopScrolling;
         }
 
         private void OnDisable()
         {
             scrollRightButton.OnHoldStart -= ScrollRight;
-            
             scrollRightButton.OnHoldEnd -= StopScrolling;
+            scrollLeftButton.OnHoldStart -= ScrollLeft;
+            scrollLeftButton.OnHoldEnd -= StopScrolling;
         }
 
         private void Update()
         {
-            scrollAction?.Invoke();
+            if (currentScroll == null) return;
+            foreach (ScrollInfo data in scrollData.Values) data.TryShowButton();
+            currentScroll?.scrollAction();
+            
+            if (currentScroll?.canButtonHide() == false) return;
+            currentScroll?.HideButton();
+            StopScrolling();
         }
 
-        public void ScrollRight() => scrollAction = () => scrollRect.horizontalNormalizedPosition += scrollSpeed;
-        public void StopScrolling() => scrollAction = null;
-
-        public ScrollRect ScrollRect { get => scrollRect; }
+        public void ScrollRight()
+        {
+            currentScroll = scrollData[DirectionType.Right];
+            currentScroll?.TryShowButton();
+        }
         
+        public void ScrollLeft()
+        {
+            currentScroll = scrollData[DirectionType.Left];
+            currentScroll?.TryShowButton();
+        }
+
+        public void StopScrolling() => currentScroll = null;
+
+        public bool IsScrollRightButtonHidden() => !scrollRightButton.gameObject.activeSelf;
+        public bool IsScrollLeftButtonHidden() => !scrollLeftButton.gameObject.activeSelf;
+        public ScrollRect ScrollRect { get => scrollRect; }
+
+        private readonly struct ScrollInfo
+        {
+            public readonly Action scrollAction;
+            public readonly Func<bool> canButtonHide;
+            private readonly HoldableButton button;
+            
+            public ScrollInfo(HoldableButton button, Action scrollAction, Func<bool> canButtonHide)
+            {
+                this.button = button;
+                this.scrollAction = scrollAction;
+                this.canButtonHide = canButtonHide;
+            }
+            
+            public void TryShowButton()
+            {
+                button.gameObject.SetActive(true);
+            }
+
+            public void HideButton() => button.gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Rogium Legend/Tests/Runtime/UI/Scrolling/ScrollButtonTests.cs
+++ Assets/Rogium Legend/Tests/Runtime/UI/Scrolling/ScrollButtonTests.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using NUnit.Framework;
+using RedRats.Core;
 using RedRats.UI.Core.Scrolling;
 using Rogium.Editors.NewAssetSelection;
 using Rogium.Tests.Core;
@@ -14,21 +15,75 @@ namespace Rogium.Tests.UI.Scrolling
     /// </summary>
     public class ScrollButtonTests : MenuTestBase
     {
+        private ScrollRectButtonAdapter adapter;
+        private ScrollRect scroll;
+
         public override IEnumerator Setup()
         {
             yield return base.Setup();
             yield return MenuLoader.PrepareSelectionMenuV2();
+            adapter = SelectionMenuOverseerMono.GetInstance().GetComponentInChildren<ScrollRectButtonAdapter>();
+            scroll = adapter.ScrollRect;
+            scroll.horizontalNormalizedPosition = 0.5f;
+            yield return null;
         }
 
         [UnityTest]
         public IEnumerator Should_ScrollCategoryTabsRight_WhenScrollRightPressed()
         {
-            ScrollRectButtonAdapter adapter = SelectionMenuOverseerMono.GetInstance().GetComponentInChildren<ScrollRectButtonAdapter>();
-            ScrollRect scroll = adapter.ScrollRect;
             float initialX = scroll.horizontalNormalizedPosition;
             adapter.ScrollRight();
             yield return new WaitForSeconds(0.1f);
             Assert.That(scroll.horizontalNormalizedPosition, Is.GreaterThan(initialX));
+        }
+        
+        [UnityTest]
+        public IEnumerator Should_StopScrollCategoryTabsRight_WhenScrollRightReleased()
+        {
+            adapter.ScrollRight();
+            yield return new WaitForSeconds(0.1f);
+            adapter.StopScrolling();
+            float stoppedValue = scroll.horizontalNormalizedPosition;
+            yield return new WaitForSeconds(0.1f);
+            Assert.That(scroll.horizontalNormalizedPosition, Is.EqualTo(stoppedValue));
+        }
+        
+        [UnityTest]
+        public IEnumerator Should_ScrollCategoryTabsLeft_WhenScrollLeftPressed()
+        {
+            float initialX = scroll.horizontalNormalizedPosition;
+            adapter.ScrollLeft();
+            yield return new WaitForSeconds(0.1f);
+            Assert.That(scroll.horizontalNormalizedPosition, Is.LessThan(initialX));
+        }
+        
+        [UnityTest]
+        public IEnumerator Should_StopScrollCategoryTabsLeft_WhenScrollLeftReleased()
+        {
+            adapter.ScrollLeft();
+            yield return new WaitForSeconds(0.1f);
+            adapter.StopScrolling();
+            float stoppedValue = scroll.horizontalNormalizedPosition;
+            yield return new WaitForSeconds(0.1f);
+            Assert.That(scroll.horizontalNormalizedPosition, Is.EqualTo(stoppedValue));
+        }
+
+        [UnityTest]
+        public IEnumerator Should_HideScrollRightButton_WhenReachedEndOfScroll()
+        {
+            adapter.ScrollRight();
+            yield return new WaitForSeconds(2f);
+            Assert.That(scroll.horizontalNormalizedPosition.IsSameAs(1f), Is.True);
+            Assert.That(adapter.IsScrollRightButtonHidden, Is.True);
+        }
+
+        [UnityTest]
+        public IEnumerator Should_HideScrollLeftButton_WhenReachedStartOfScroll()
+        {
+            adapter.ScrollLeft();
+            yield return new WaitForSeconds(2f);
+            Assert.That(scroll.horizontalNormalizedPosition.IsSameAs(0f), Is.True);
+            Assert.That(adapter.IsScrollLeftButtonHidden, Is.True);
         }
     }
 }
