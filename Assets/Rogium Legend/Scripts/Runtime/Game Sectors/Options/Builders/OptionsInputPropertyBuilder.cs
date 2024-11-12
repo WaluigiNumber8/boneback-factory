b5e0409306d31734c1a6c1eda6d0332168e8f1af
shyguy255@seznam.cz <>
2024-11-12 16:52:14 +0100

    BBF-379 Add Click and player buttons press inputs to Input Options

--- Assets/Rogium Legend/Scripts/Runtime/Game Sectors/Options/Builders/OptionsInputPropertyBuilder.cs
+++ Assets/Rogium Legend/Scripts/Runtime/Game Sectors/Options/Builders/OptionsInputPropertyBuilder.cs
@@ -9,14 +9,25 @@ namespace Rogium.Options.Core
     /// </summary>
     public class OptionsInputPropertyBuilder : UIPropertyContentBuilderBaseColumn1<GameDataAsset>
     {
-        public OptionsInputPropertyBuilder(Transform contentMain) : base(contentMain) { }
+        private readonly InputSystem input;
+
+        public OptionsInputPropertyBuilder(Transform contentMain) : base(contentMain) => input = InputSystem.GetInstance();
 
         public override void Build(GameDataAsset data)
         {
             Clear();
             
             b.BuildHeader("UI", contentMain);
-            b.BuildInputBinding(InputSystem.GetInstance().UI.ClickAlternative.Action, contentMain);
+            b.BuildInputBinding(input.UI.Click.Action, contentMain);
+            b.BuildInputBinding(input.UI.ClickAlternative.Action, contentMain);
+            
+            b.BuildHeader("Player", contentMain);
+            b.BuildInputBinding(input.Player.ButtonMain.Action, contentMain);
+            b.BuildInputBinding(input.Player.ButtonMainAlt.Action, contentMain);
+            b.BuildInputBinding(input.Player.ButtonSub.Action, contentMain);
+            b.BuildInputBinding(input.Player.ButtonSubAlt.Action, contentMain);
+            b.BuildInputBinding(input.Player.ButtonDash.Action, contentMain);
+            b.BuildInputBinding(input.Player.ButtonDashAlt.Action, contentMain);
         }
     }
 }
