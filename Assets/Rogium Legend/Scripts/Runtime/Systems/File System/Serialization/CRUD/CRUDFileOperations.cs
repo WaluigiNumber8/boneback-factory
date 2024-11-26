1e82f7dccc78766421febb425bc0afa18fcbd5cd
shyguy255@seznam.cz <>
2024-11-26 18:19:36 +0100

    BBF-381 Maked saved JSON be pretty

---
+++ Assets/Rogium Legend/Scripts/Runtime/Systems/File System/Serialization/CRUD/CRUDFileOperations.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using RedRats.Core;
+using RedRats.Systems.FileSystem;
+
+namespace Rogium.ExternalStorage
+{
+    /// <summary>
+    /// CRUD operations for JSON files.
+    /// </summary>
+    public class CRUDFileOperations : ICRUDFileOperations
+    {
+        private const string JsonExtension = ".json";
+
+        private readonly string path;
+        private readonly string folderPath;
+        private readonly bool prettify;
+
+        public CRUDFileOperations(string title, string folderPath, bool prettify = true)
+        {
+            this.prettify = prettify;
+            this.folderPath = folderPath;
+            path = Path.Combine(this.folderPath, $"{title}{JsonExtension}");
+            if (!File.Exists(path)) Save("");
+        }
+
+        public void Save(string data)
+        {
+            FileSystem.CreateDirectory(folderPath);
+            string finalData = prettify ? data.AsPrettyJson() : data;
+            FileSystem.SaveFile(path, finalData);
+        }
+
+        public string Load() => FileSystem.LoadFile(path, JsonExtension);
+
+        public void Delete() => FileSystem.DeleteFile(path);
+    }
+}
